07c556e2464f3ce49fc314217e92c30b2762b743
metal99 <metal99@gmail.com>
2014-10-14 22:05:53 -0600

    first commit

---
+++ TODOList/Controllers/TODOListController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TODOList.Controllers
+{
+    public class TODOListController : Controller
+    {
+        // GET: TODOList
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TODOList/Models/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TODOList.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string TODOItem { get; set; }
+    }
+}
